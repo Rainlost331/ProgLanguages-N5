e89c4fba49804cc00d792e3286055ee5c06c9fb0
Rainlost331 <artem2004d04@mail.ru>
2024-03-08 13:26:12 +0300

    Связывание модификаторов

--- app/src/main/java/ui/components/NoteColor.kt
+++ app/src/main/java/ui/components/NoteColor.kt
@@ -3,8 +3,10 @@ package ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -13,7 +15,9 @@ import androidx.compose.ui.unit.dp
 fun NoteColor(){
     Box(modifier = Modifier
         .size(40.dp)
-        .background(Color.Red))
+        .clip(CircleShape)
+        .background(Color.Red)
+        )
 }
 
 @Preview
