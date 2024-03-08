c6ef48b3496b9da894c1a77a7f0020950bfe2ba5
Rainlost331 <artem2004d04@mail.ru>
2024-03-08 14:02:03 +0300

    Предварительный просмотр ColorItem

--- app/src/main/java/ui/components/screens/SaveNoteScreen.kt
+++ app/src/main/java/ui/components/screens/SaveNoteScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.topic2.android.notes.domain.model.ColorModel
@@ -37,4 +38,10 @@ fun ColorItem(
                 .align(Alignment.CenterVertically)
         )
     }
+}
+
+@Preview
+@Composable
+fun ColorItemPreview(){
+    ColorItem(ColorModel.DEFAULT) {}
 }
